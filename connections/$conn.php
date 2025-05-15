c5bfdfc1920d9a1ce8d595bc3f1357797b8ab3be
ray.brugnari <ray.brugnari@gmail.com>
2025-05-15 03:35:06 -0300

    removendo linha desnecessaria

--- connections/$conn.php
+++ connections/$conn.php
@@ -4,8 +4,6 @@ $user = "root";
 $pass = "adimin";
 $bd = "site-praticando";
 
-
 $conn= new PDO("mysql:host=$host;dbname=$bd;charset=utf8", $user, $pass);
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-C:\xampp\htdocs\ExRob\bloco de notas inteligente
